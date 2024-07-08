df6ba756b937a9f4a8bd711b194459ca8f7d473c
angelloll <160196777+angelloll@users.noreply.github.com>
2024-07-08 05:40:50 +0200

    Update system.sh

--- system.sh
+++ system.sh
@@ -60,10 +60,11 @@ echo -e "$GREEN Done...$COL_RESET"
 # MariaDB
 echo -e " Installing MariaDB Repository...$COL_RESET"
 hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-if [[ ("$DISTRO" == "16") ]]; then
-  sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu xenial main' >/dev/null 2>&1
-else
+if [[ ("$DISTRO" == "20") ]]; then
   sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu bionic main' >/dev/null 2>&1
+else
+   sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el,s390x] http://mirror.one.com/mariadb/repo/10.4/ubuntu focal main' >/dev/null 2>&1
+
 fi
 echo -e "$GREEN Done...$COL_RESET"
 
